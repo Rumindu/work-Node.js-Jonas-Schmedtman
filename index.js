9acef5e4dfa99b81d31c739375ce4ec43209055c
M. R. K. W. DE SILVA <108813657+Rumindu@users.noreply.github.com>
2023-12-18 18:22:08 +0530

    deleted files

--- index.js
+++
@@ -1,4 +0,0 @@
-const fs =require ('fs');
-const text = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(text);
-fs.writeFileSync('./txt/output.txt', `This is what we know about the avocado: ${text}.\nCreated on ${Date.now()}`);
